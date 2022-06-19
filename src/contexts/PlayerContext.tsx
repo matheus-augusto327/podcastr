dc2e588d82f899fd4c0bd4b6a2e6abfbcddcfc84
Matheus <matheus.augustoreal@gmail.com>
2022-06-19 10:05:43 -0300

    Loop control

--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -12,10 +12,12 @@ type PlayerContextData = {
   episodeList: Episode[];
   currentEpisodeIndex: number;
   isPlaying: boolean;
+  isLooping: boolean;
   play: (episode: Episode) => void;
   playList: (episode: Episode[], index: number) => void;
   setPlayingState: (state: boolean) => void;
   togglePlay: () => void;
+  toggleLoop: () => void;
   playNext: () => void;
   playPrevious: () => void;
   hasNext: boolean;
@@ -32,6 +34,7 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
   const [episodeList, setEpisodeList] = useState([])
   const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0)
   const [isPlaying, setIsPlaying] = useState(false)
+  const [isLooping, setIsLooping] = useState(false)
 
   function play(episode: Episode) {
     setEpisodeList([episode])
@@ -47,6 +50,10 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
 
   function togglePlay() {
     setIsPlaying(!isPlaying)
+  }
+
+  function toggleLoop() {
+    setIsLooping(!isLooping)
   }
 
   function setPlayingState(state: boolean) {
@@ -78,7 +85,9 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
         playPrevious,
         playNext,
         isPlaying,
+        isLooping,
         togglePlay,
+        toggleLoop,
         setPlayingState,
         hasNext,
         hasPrevious
